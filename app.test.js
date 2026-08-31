87206b11f039ce098fb900e2bfa152a0a034e679
kaua vinicius <kauavin2007@gmail.com>
2026-08-31 16:28:47 -0300

    voltando ao normal

--- app.test.js
+++ app.test.js
@@ -5,6 +5,6 @@ describe("API Data e Hora", ()=> {
     test("GET / deve retornar HTTP 200", async ()=> {
         const response = await request (app).get ("/");
 
-        expect(response.statusCode).toBe(201);
+        expect(response.statusCode).toBe(200);
     });
 });
